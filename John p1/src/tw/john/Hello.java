4f2a7165bf9b54df9fbde31292a4339356a2e660
Mac <Mac@Mac-PC>
2017-03-05 13:25:21 +0800

    java lesson 1

---
+++ John p1/src/tw/john/Hello.java
@@ -0,0 +1,9 @@
+package tw.john;
+
+public class John00
+{
+	public static void main (String[] john)	
+	{
+		System.out.println("Hello, World");
+	}
+}
---
+++ John p1/src/tw/john/John01.java
@@ -0,0 +1,20 @@
+package tw.john;
+
+public class John01 
+{
+	/*
+	 * 很多註解.....
+	 * 
+	 */
+	/**
+	 * javadoc用
+	 * @param args
+	 */
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
